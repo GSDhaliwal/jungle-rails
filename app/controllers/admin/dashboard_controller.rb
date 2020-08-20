08b389c3d3ae7e3b17fc086d0bdd4d5c92b6a93b
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-08-20 17:31:47 +0000

    add admin categories section

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+  
+  Rails.configuration.authentication = {
+    :username => ENV['USERNAME'],
+    :password => ENV['PASSWORD']
+  }
+
+  http_basic_authenticate_with name: Rails.configuration.authentication[:username], password: Rails.configuration.authentication[:password]
+  
   def show
     @products = Product.all
     @categories = Category.all
